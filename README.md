7aa8e2cba627bf61277f5c0460826cb3288f49ba
peanutyabing <sherry.liuyabing@gmail.com>
2023-04-29 18:01:21 +0800

    Create directories and configure database connection

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# flash-backend
+# Rocket Academy Coding Bootcamp: Capstone Project Flash Backend
+
+Run `npm i` to install NPM packages, then `npm start` to start the Express server.
---
+++ index.js
@@ -0,0 +1,34 @@
+const cors = require("cors");
+const express = require("express");
+
+require("dotenv").config();
+const PORT = process.env.PORT;
+const app = express();
+
+const corsOptions = {
+  origin: "http://localhost:3001",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// importing Routers
+
+// importing Controllers
+
+// importing DB
+// const db = require("./db/models/index.js");
+// const { model1, model2 } = db;
+
+// initializing Controllers -> note the lowercase for the first word
+
+// inittializing Routers
+
+// using routers
+
+const http = require("http").Server(app);
+http.listen(PORT, () => {
+  console.log(`Express app listening on port ${PORT}!`);
+});
