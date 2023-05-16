25a371b4166231e151d46716b52f30e4c5139e3c
peanutyabing <sherry.liuyabing@gmail.com>
2023-05-16 20:21:41 +0800

    Update seeders to include more deck and cards data

--- db/seeders/20230502072135-deck_subcategories.js
+++ db/seeders/20230502072135-deck_subcategories.js
@@ -18,6 +18,24 @@ module.exports = {
           created_at: new Date("2023-04-01"),
           updated_at: new Date("2023-04-01"),
         },
+        {
+          deck_id: 13,
+          subcategory_id: 1,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          deck_id: 14,
+          subcategory_id: 1,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          deck_id: 15,
+          subcategory_id: 1,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
       ],
       {}
     );
